4ecb269dfdb8e993af447d9ef10c8bdddaa8762c
TheOneTrueZandra <aaron.mich.mills@gmail.com>
2021-03-22 17:05:04 -0600

    Added functionality to the world API

--- src/main/java/net/blancworks/figura/lua/api/world/WorldAPI.java
+++ src/main/java/net/blancworks/figura/lua/api/world/WorldAPI.java
@@ -7,16 +7,25 @@ import net.blancworks.figura.lua.api.ReadOnlyLuaTable;
 import net.blancworks.figura.lua.api.world.block.BlockStateAPI;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.nbt.NbtHelper;
 import net.minecraft.particle.DefaultParticleType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.LightType;
 import net.minecraft.world.World;
-import org.luaj.vm2.LuaTable;
-import org.luaj.vm2.LuaValue;
+import net.minecraft.world.biome.Biome;
+import org.luaj.vm2.*;
 import org.luaj.vm2.lib.OneArgFunction;
 import org.luaj.vm2.lib.ThreeArgFunction;
+import org.luaj.vm2.lib.ZeroArgFunction;
 
 public class WorldAPI {
+    
+    private static World getWorld(){
+        return MinecraftClient.getInstance().world;
+    }
 
     private static final ReadOnlyLuaTable globalLuaTable = new ReadOnlyLuaTable(new LuaTable() {{
         
@@ -28,13 +37,96 @@ public class WorldAPI {
 
                 if(pos == null) return NIL;
                 
-                World w = MinecraftClient.getInstance().world;
+                World w = getWorld();
                 
                 if(!w.isChunkLoaded(pos)) return NIL;
                 
                 BlockState state = w.getBlockState(pos);
                 
                 return BlockStateAPI.getTable(state);
+            }
+        });
+
+
+        set("getTime", new ZeroArgFunction() {
+            @Override
+            public LuaValue call() {
+                return LuaNumber.valueOf(getWorld().getTime());
+            }
+        });
+
+
+        set("getTimeOfDay", new ZeroArgFunction() {
+            @Override
+            public LuaValue call() {
+                return LuaNumber.valueOf(getWorld().getTimeOfDay());
+            }
+        });
+
+        set("getMoonPhase", new ZeroArgFunction() {
+            @Override
+            public LuaValue call() {
+                return LuaNumber.valueOf(getWorld().getMoonPhase());
+            }
+        });
+
+        set("getLunarTime", new ZeroArgFunction() {
+            @Override
+            public LuaValue call() {
+                return LuaNumber.valueOf(getWorld().getLunarTime());
+            }
+        });
+
+        set("getRainGradient", new OneArgFunction() {
+            @Override
+            public LuaValue call(LuaValue a) {
+                return LuaNumber.valueOf(getWorld().getRainGradient((float)(a.checkdouble())));
+            }
+        });
+
+        set("getThunderGradient", new OneArgFunction() {
+            @Override
+            public LuaValue call(LuaValue a) {
+                return LuaNumber.valueOf(getWorld().getThunderGradient((float)(a.checkdouble())));
+            }
+        });
+
+        set("getLightLevel", new OneArgFunction() {
+            @Override
+            public LuaValue call(LuaValue a) {
+                BlockPos pos = LuaUtils.getBlockPosFromTable(a.checktable());
+                
+                if(pos == null)
+                    return LuaInteger.valueOf(0);
+                
+                int block = getWorld().getLightLevel(LightType.BLOCK, pos);
+                int sky = getWorld().getLightLevel(LightType.SKY, pos);
+                
+                int ambientDark = getWorld().getAmbientDarkness();
+                
+                
+                
+                return LuaInteger.valueOf(Math.max(block, sky));
+            }
+        });
+
+        set("getBiomeID", new OneArgFunction() {
+            @Override
+            public LuaValue call(LuaValue a) {
+
+                BlockPos pos = LuaUtils.getBlockPosFromTable(a.checktable());
+
+                if(pos == null)
+                    return NIL;
+                
+                Biome b = getWorld().getBiome(pos);
+                
+                if(b == null)
+                    return NIL;
+                
+                Identifier id = getWorld().getRegistryManager().get(Registry.BIOME_KEY).getId(b);
+                
+                return LuaString.valueOf(id.toString());
             }
         });
         
