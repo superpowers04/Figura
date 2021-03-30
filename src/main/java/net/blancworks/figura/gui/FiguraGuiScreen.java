8e7d686916a94ab685202b014fd957d192da0ed0
Francy-chan <40085587+Francy-chan@users.noreply.github.com>
2021-03-30 20:57:49 -0300

    UI + Model Load
    * Fixed remaining UI issues
    * Added .zip and folder support
    * Localized all the texts

--- src/main/java/net/blancworks/figura/gui/FiguraGuiScreen.java
+++ src/main/java/net/blancworks/figura/gui/FiguraGuiScreen.java
@@ -7,7 +7,6 @@ import net.blancworks.figura.gui.widgets.CustomListWidgetState;
 import net.blancworks.figura.gui.widgets.ModelFileListWidget;
 import net.blancworks.figura.network.FiguraNetworkManager;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
@@ -25,7 +24,10 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.Quaternion;
 
-import java.util.Objects;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 import java.util.concurrent.CompletableFuture;
 
 public class FiguraGuiScreen extends Screen {
@@ -39,6 +41,7 @@ public class FiguraGuiScreen extends Screen {
     public TexturedButtonWidget uploadButton;
 
     public MutableText name_text;
+    public MutableText raw_name_text;
     public MutableText file_size_text;
     public MutableText model_complexity_text;
 
@@ -49,44 +52,84 @@ public class FiguraGuiScreen extends Screen {
     private int rightPaneX;
     private int searchBoxX;
     private int filtersX;
-    
+
+    //gui sizes
+    private int guiScale, modelBgSize, modelSize;
+
+    //model rotation
+    private double anchorX, anchorY;
+    private double anchorAngleX, anchorAngleY;
+    private double angleX, angleY;
+    private boolean canRotate;
+
     public FiguraTrustScreen trustScreen = new FiguraTrustScreen(this);
     
     public CustomListWidgetState modelFileListState = new CustomListWidgetState();
     public ModelFileListWidget modelFileList;
 
     public FiguraGuiScreen(Screen parentScreen) {
-        super(new LiteralText("Figura Menu"));
+        super(new TranslatableText("gui.figura.menutitle"));
         this.parentScreen = parentScreen;
+
+        //reset model rotation
+        anchorX = 0.0D;
+        anchorY = 0.0D;
+        anchorAngleX = 0.0D;
+        anchorAngleY = 0.0D;
+        angleX = -15.0D;
+        angleY = 30.0D;
     }
 
     @Override
     protected void init() {
         super.init();
 
+        //model size
+        guiScale = (int) this.client.getWindow().getScaleFactor();
+        double screenScale = Math.min(this.width, this.height) / 1018.0;
+        modelBgSize = (int) ((512 / guiScale) * (screenScale * guiScale));
+        modelSize = (int) ((192 / guiScale)   * (screenScale * guiScale));
+
+        //search box and model list
+        paneY = 48;
+        paneWidth = this.width / 3 - 8;
+        rightPaneX = paneWidth + 10;
+
+        int searchBoxWidth = paneWidth - 5;
+        searchBoxX = 7;
+        this.searchBox = new TextFieldWidget(this.textRenderer, searchBoxX, 22, searchBoxWidth, 20, this.searchBox, new TranslatableText("gui.figura.button.search"));
+        this.searchBox.setChangedListener((string_1) -> this.modelFileList.filter(string_1, false));
+        modelFileList = new ModelFileListWidget(this.client, paneWidth, this.height, paneY + 19, this.height - 36, 20, this.searchBox, this.modelFileList, this, modelFileListState);
+        this.modelFileList.setLeftPos(5);
+        this.addChild(modelFileList);
+        this.addChild(searchBox);
+
         int width = Math.min((this.width / 2) - 10 - 128, 128);
-        
-        this.addButton(new ButtonWidget(this.width - width - 5, this.height - 20 - 5, width, 20, new LiteralText("Back"), (buttonWidgetx) -> {
-            this.client.openScreen(parentScreen);
-        }));
 
-        this.addButton(new ButtonWidget(this.width - width - 5, this.height - 42 - 5, width, 20, new LiteralText("Trust Menu"), (buttonWidgetx) -> {
-            this.client.openScreen(trustScreen);
-        }));
-
-        this.addButton(new ButtonWidget(this.width - width - 5, this.height - 64 - 5, width, 20, new LiteralText("Help"), (buttonWidgetx) -> {
-            Util.getOperatingSystem().open("https://github.com/TheOneTrueZandra/Figura/wiki/Figura-Panel");
-        }));
-
-        this.addButton(new ButtonWidget(this.width - width - 5, this.height - 86 - 5, width, 20, new LiteralText("Open Files Folder"), (buttonWidgetx) -> {
+        //open folder
+        this.addButton(new ButtonWidget(5, this.height - 20 - 5, 140, 20, new TranslatableText("gui.figura.button.openfolder"), (buttonWidgetx) -> {
             Util.getOperatingSystem().open(FiguraMod.getModContentDirectory().toUri());
         }));
 
+        //back button
+        this.addButton(new ButtonWidget(this.width - width - 5, this.height - 20 - 5, width, 20, new TranslatableText("gui.figura.button.back"), (buttonWidgetx) -> {
+            this.client.openScreen(parentScreen);
+        }));
+
+        //top buttons
+        this.addButton(new ButtonWidget(this.width - 140 - 5, 15, 140, 20, new TranslatableText("gui.figura.button.trustmenu"), (buttonWidgetx) -> {
+            this.client.openScreen(trustScreen);
+        }));
+        this.addButton(new ButtonWidget(this.width - 140 - 5, 40, 140, 20, new TranslatableText("gui.figura.button.help"), (buttonWidgetx) -> {
+            Util.getOperatingSystem().open("https://github.com/TheOneTrueZandra/Figura/wiki/Figura-Panel");
+        }));
+
+        //upload button
         uploadButton = new TexturedButtonWidget(
-                this.width - 32 - 10, 5,
-                32, 32,
-                0, 0, 32,
-                uploadTexture, 32, 64,
+                this.width / 2 + modelBgSize / 2 + 4, this.height / 2 + modelBgSize / 2 - 32,
+                25, 25,
+                0, 0, 25,
+                uploadTexture, 25, 50,
                 (bx) -> {
                     FiguraNetworkManager.postModel();
                 }
@@ -94,55 +137,56 @@ public class FiguraGuiScreen extends Screen {
         this.addButton(uploadButton);
         
         if (PlayerDataManager.localPlayer != null && PlayerDataManager.localPlayer.model != null) {
-            model_complexity_text = new LiteralText(String.format("Complexity : %d", PlayerDataManager.localPlayer.model.getRenderComplexity()));
+            model_complexity_text = new TranslatableText("gui.figura.complexity", PlayerDataManager.localPlayer.model.getRenderComplexity());
             file_size_text = getFileSizeText();
         }
-
-        paneY = 48;
-        paneWidth = this.width / 2 - 8;
-        rightPaneX = width - paneWidth;
-
-        int searchBoxWidth = paneWidth - 5 - 5;
-        searchBoxX = 5;
-        this.searchBox = new TextFieldWidget(this.textRenderer, searchBoxX, 22, searchBoxWidth, 20, this.searchBox, new TranslatableText("modmenu.search"));
-        this.searchBox.setChangedListener((string_1) -> this.modelFileList.filter(string_1, false));
-        modelFileList = new ModelFileListWidget(this.client, paneWidth, this.height, paneY , this.height - 5, 14, this.searchBox, this.modelFileList, this, modelFileListState);
-        this.addChild(modelFileList);
-        this.addChild(searchBox);
     }
 
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         renderBackground(matrices);
-        super.render(matrices, mouseX, mouseY, delta);
-        
-        //Draw player preview.
-        {
-            MinecraftClient.getInstance().getTextureManager().bindTexture(playerBackgroundTexture);
-            drawTexture(matrices, this.width / 2, this.height / 2 - (128), 0, 0, 128, 128, 128, 128);
 
-            drawEntity(this.width / 2 + 64, this.height / 2 - 32, 32, (this.width / 2 + 64) - mouseX, (this.height / 2 - 32 - 50) - mouseY, MinecraftClient.getInstance().player);
+        //draw player preview
+        {
+            RenderSystem.setShader(GameRenderer::getPositionTexColorShader);
+            RenderSystem.setShaderTexture(0, playerBackgroundTexture);
+            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+
+            drawTexture(matrices, this.width / 2 - modelBgSize / 2, this.height / 2 - modelBgSize / 2, 0, 0, modelBgSize, modelBgSize, modelBgSize, modelBgSize);
+            drawEntity(this.width / 2, this.height / 2, modelSize, (float) angleX, (float) angleY, MinecraftClient.getInstance().player);
         }
 
-        //Draw avatar info
+        //draw avatar info
         {
-            MinecraftClient.getInstance().getTextureManager().bindTexture(playerBackgroundTexture);
-            drawTexture(matrices, this.width / 2, this.height / 2, 0, 0, 128, 128, 128, 128);
+            RenderSystem.setShader(GameRenderer::getPositionTexColorShader);
+            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
 
-            int currY = this.height / 2 + 4 - 12;
+            int currY = 45 + 12;
 
-            if (name_text != null)
-                drawTextWithShadow(matrices, MinecraftClient.getInstance().textRenderer, name_text, this.width / 2 + 4, currY += 12, 16777215);
+            if (name_text != null) {
+                drawTextWithShadow(matrices, MinecraftClient.getInstance().textRenderer, name_text, this.width - this.textRenderer.getWidth(name_text) - 8, currY += 12, 16777215);
+                drawCenteredText(matrices, MinecraftClient.getInstance().textRenderer, raw_name_text, this.width / 2, this.height / 2 - modelBgSize / 2 - 12, 16777215);
+            }
             if (file_size_text != null)
-                drawTextWithShadow(matrices, MinecraftClient.getInstance().textRenderer, file_size_text, this.width / 2 + 4, currY += 12, 16777215);
+                drawTextWithShadow(matrices, MinecraftClient.getInstance().textRenderer, file_size_text, this.width - this.textRenderer.getWidth(file_size_text) - 8, currY += 12, 16777215);
             if (model_complexity_text != null)
-                drawTextWithShadow(matrices, MinecraftClient.getInstance().textRenderer, model_complexity_text, this.width / 2 + 4, currY += 12, 16777215);
+                drawTextWithShadow(matrices, MinecraftClient.getInstance().textRenderer, model_complexity_text, this.width - this.textRenderer.getWidth(model_complexity_text) - 8, currY += 12, 16777215);
 
             if (this.getFocused() != null)
                 FiguraMod.LOGGER.debug(this.getFocused().toString());
         }
+
+        //draw search box and file list
         modelFileList.render(matrices, mouseX, mouseY, delta);
         searchBox.render(matrices, mouseX, mouseY, delta);
+
+        //deprecated warning
+        if (raw_name_text != null)
+            if (raw_name_text.getString().endsWith("*"))
+                drawCenteredText(matrices, MinecraftClient.getInstance().textRenderer,  new TranslatableText("gui.figura.deprecatedwarning"), this.width / 2, 4, TextColor.parse("red").getRgb());
+
+        //draw buttons
+        super.render(matrices, mouseX, mouseY, delta);
     }
 
     @Override
@@ -154,7 +198,8 @@ public class FiguraGuiScreen extends Screen {
     static void overlayBackground(int x1, int y1, int x2, int y2, int red, int green, int blue, int startAlpha, int endAlpha) {
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder buffer = tessellator.getBuffer();
-        Objects.requireNonNull(MinecraftClient.getInstance()).getTextureManager().bindTexture(DrawableHelper.OPTIONS_BACKGROUND_TEXTURE);
+        RenderSystem.setShader(GameRenderer::getPositionTexColorShader);
+        RenderSystem.setShaderTexture(0, OPTIONS_BACKGROUND_TEXTURE);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE_COLOR);
         buffer.vertex(x1, y2, 0.0D).texture(x1 / 32.0F, y2 / 32.0F).color(red, green, blue, endAlpha).next();
@@ -171,7 +216,8 @@ public class FiguraGuiScreen extends Screen {
         PlayerDataManager.lastLoadedFileName = file_name;
         PlayerDataManager.localPlayer.loadModelFile(file_name);
 
-        name_text = new LiteralText(String.format("Name : %s", file_name.substring(0, Math.min(15, file_name.length()))));
+        name_text = new TranslatableText("gui.figura.name", file_name.substring(0, Math.min(20, file_name.length())));
+        raw_name_text = new LiteralText(file_name);
 
         CompletableFuture.runAsync(() -> {
 
@@ -186,7 +232,7 @@ public class FiguraGuiScreen extends Screen {
                 }
             }
 
-            model_complexity_text = new LiteralText(String.format("Complexity : %d", PlayerDataManager.localPlayer.model.getRenderComplexity()));
+            model_complexity_text = new TranslatableText("gui.figura.complexity", PlayerDataManager.localPlayer.model.getRenderComplexity());
             file_size_text = getFileSizeText();
         }, Util.getMainWorkerExecutor());
 
@@ -195,8 +241,13 @@ public class FiguraGuiScreen extends Screen {
     public MutableText getFileSizeText() {
         int fileSize = PlayerDataManager.localPlayer.getFileSize();
 
+        //format file size
+        DecimalFormat df = new DecimalFormat("#0.00", new DecimalFormatSymbols(Locale.US));
+        df.setRoundingMode(RoundingMode.HALF_UP);
+        float size = Float.parseFloat(df.format(fileSize / 1000.0f));
+        long i = (long) size;
 
-        MutableText fsText = new LiteralText(String.format("File Size : %.2fkB", fileSize / 1000.0f));
+        MutableText fsText = new TranslatableText("gui.figura.filesize", fileSize == i ? String.valueOf(i) : String.valueOf(size));
 
         if (fileSize >= filesize_large_threshold)
             fsText.setStyle(fsText.getStyle().withColor(TextColor.parse("red")));
@@ -208,6 +259,107 @@ public class FiguraGuiScreen extends Screen {
         return fsText;
     }
 
-    public static void drawEntity(int x, int y, int size, float mouseX, float mouseY, LivingEntity entity) {
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        //set anchor rotation
+        if (mouseX >= this.width / 2.0 - modelBgSize / 2.0 && mouseX <= this.width / 2.0 + modelBgSize / 2.0) {
+            if (mouseY >= this.height / 2.0 - modelBgSize / 2.0 && mouseY <= this.height / 2.0 + modelBgSize / 2.0) {
+                //get starter mouse pos
+                anchorX = mouseX;
+                anchorY = mouseY;
+
+                //get starter rotation angles
+                anchorAngleX = angleX;
+                anchorAngleY = angleY;
+
+                canRotate = true;
+            }
+        }
+        return super.mouseClicked(mouseX, mouseY, button);
+    }
+
+    @Override
+    public boolean mouseReleased(double mouseX, double mouseY, int button) {
+        //reset rotate ability
+        canRotate = false;
+        return super.mouseReleased(mouseX, mouseY, button);
+    }
+
+    @Override
+    public boolean mouseDragged(double mouseX, double mouseY, int button, double deltaX, double deltaY) {
+        //set rotations
+        if (canRotate) {
+            //get starter rotation angle then get hot much is moved and divided by a slow factor
+            angleX = anchorAngleX + (anchorY - mouseY) / 1.5;
+            angleY = anchorAngleY - (anchorX - mouseX) / 1.5;
+
+            //prevent rating so much down and up
+            if (angleX > 90) {
+                anchorY = mouseY;
+                anchorAngleX = 90;
+                angleX = 90;
+            } else if (angleX < -90) {
+                anchorY = mouseY;
+                anchorAngleX = -90;
+                angleX = -90;
+            }
+            //cap to 360 so we don't get extremely high unnecessary rotation values
+            if (angleY >= 360 || angleY <= -360) {
+                anchorX = mouseX;
+                anchorAngleY = 0;
+                angleY = 0;
+            }
+        }
+
+        return super.mouseDragged(mouseX, mouseY, button, deltaX, deltaY);
+    }
+
+    public static void drawEntity(int x, int y, int size, float rotationX, float rotationY, LivingEntity entity) {
+        MatrixStack matrixStack = RenderSystem.getModelViewStack();
+        matrixStack.push();
+        matrixStack.translate((float) x, (float) y, 1500.0F);
+        matrixStack.scale(1.0F, 1.0F, -1.0F);
+        RenderSystem.applyModelViewMatrix();
+        MatrixStack matrixStack2 = new MatrixStack();
+        matrixStack2.translate(0.0D, 0.0D, 1000.0D);
+        matrixStack2.scale((float) size, (float) size, (float) size);
+        Quaternion quaternion = Vec3f.POSITIVE_Z.getDegreesQuaternion(180.0F);
+        Quaternion quaternion2 = Vec3f.POSITIVE_X.getDegreesQuaternion(rotationX);
+        Quaternion quaternion3 = Vec3f.POSITIVE_Y.getDegreesQuaternion(rotationY);
+        quaternion.hamiltonProduct(quaternion2);
+        quaternion.hamiltonProduct(quaternion3);
+        matrixStack2.multiply(quaternion);
+        float h = entity.bodyYaw;
+        float i = entity.yaw;
+        float j = entity.pitch;
+        float k = entity.prevHeadYaw;
+        float l = entity.headYaw;
+        boolean invisible = entity.isInvisible();
+        entity.bodyYaw = 180.0F;
+        entity.yaw = 180.0F;
+        entity.pitch = 0.0F;
+        entity.headYaw = entity.yaw;
+        entity.prevHeadYaw = entity.yaw;
+        entity.setInvisible(false);
+        DiffuseLighting.method_34742();
+        EntityRenderDispatcher entityRenderDispatcher = MinecraftClient.getInstance().getEntityRenderDispatcher();
+        quaternion2.conjugate();
+        entityRenderDispatcher.setRotation(quaternion2);
+        entityRenderDispatcher.setRenderShadows(false);
+        VertexConsumerProvider.Immediate immediate = MinecraftClient.getInstance().getBufferBuilders().getEntityVertexConsumers();
+        RenderSystem.runAsFancy(() -> {
+            entityRenderDispatcher.render(entity, 0.0D, -1.0D, 0.0D, 0.0F, 1.0F, matrixStack2, immediate, 15728880);
+        });
+        immediate.draw();
+        entityRenderDispatcher.setRenderShadows(true);
+        entity.bodyYaw = h;
+        entity.yaw = i;
+        entity.pitch = j;
+        entity.prevHeadYaw = k;
+        entity.headYaw = l;
+        entity.setInvisible(invisible);
+        matrixStack.pop();
+        RenderSystem.applyModelViewMatrix();
+        DiffuseLighting.enableGuiDepthLighting();
     }
 }
--- src/main/java/net/blancworks/figura/gui/widgets/ModelFileListWidget.java
+++ src/main/java/net/blancworks/figura/gui/widgets/ModelFileListWidget.java
@@ -13,6 +13,7 @@ import org.apache.commons.io.FilenameUtils;
 import java.io.File;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.zip.ZipFile;
 
 public class ModelFileListWidget extends CustomListWidget<PlayerListEntry, ModelFileListWidget.ModelFileListWidgetEntry> {
 
@@ -33,25 +34,47 @@ public class ModelFileListWidget extends CustomListWidget<PlayerListEntry, Model
             e.printStackTrace();
         }
 
-        ArrayList<String> valid_loads = new ArrayList<String>();
-
         File[] files = contentDirectory.listFiles();
 
         for (File file : files) {
+            //get file name
             String fileName = FilenameUtils.removeExtension(file.getName());
 
-            if (!fileName.contains(searchTerm))
+            //skip files
+            if (!fileName.contains(searchTerm) || !(file.getName().endsWith(".zip") || file.isDirectory() || !file.getName().endsWith(".bbmodel")))
                 continue;
 
-            if (Files.exists(contentDirectory.toPath().resolve(fileName + ".bbmodel")) && Files.exists(contentDirectory.toPath().resolve(fileName + ".png"))) {
-                if (valid_loads.contains(fileName))
-                    continue;
-                valid_loads.add(fileName);
+            //if directory
+            if (file.isDirectory()) {
+                //add entry
+                if (Files.exists(file.toPath().resolve("model.bbmodel")) && Files.exists(file.toPath().resolve("texture.png"))) {
+                    addEntry(new ModelFileListWidgetEntry(file.getName(), this));
+                }
             }
-        }
+            //zip support
+            else if (file.getName().endsWith(".zip")) {
+                try {
+                    ZipFile zipFile = new ZipFile(file.getPath());
 
-        for (String valid_load : valid_loads) {
-            addEntry(new ModelFileListWidgetEntry(valid_load, this));
+                    boolean hasModel = zipFile.getEntry("model.bbmodel") != null;
+                    boolean hasTexture = zipFile.getEntry("texture.png") != null;
+
+                    //add entry
+                    if (hasModel && hasTexture) {
+                        addEntry(new ModelFileListWidgetEntry(file.getName(), this));
+                    }
+                } catch (Exception e) {
+                    System.out.println("Failed to load model " + file.getAbsolutePath());
+                    e.printStackTrace();
+                }
+            }
+            //old system compatibility
+            else {
+                //add entry
+                if (Files.exists(contentDirectory.toPath().resolve(fileName + ".png"))) {
+                    addEntry(new ModelFileListWidgetEntry(fileName + "*", this));
+                }
+            }
         }
     }
 
