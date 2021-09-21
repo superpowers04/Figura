3cd47355081e9d6d1e4bb8863d7d06d2750026c2
Fran <40085587+Francy-chan@users.noreply.github.com>
2021-09-20 22:41:10 -0300

    Update InGameHudMixin.java

--- src/main/java/net/blancworks/figura/mixin/InGameHudMixin.java
+++ src/main/java/net/blancworks/figura/mixin/InGameHudMixin.java
@@ -10,9 +10,9 @@ import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
@@ -39,34 +39,27 @@ public class InGameHudMixin {
     private void renderCrosshair(MatrixStack matrices, CallbackInfo ci) {
         if (FiguraMod.actionWheel.isPressed() && ActionWheel.enabled)
             ci.cancel();
+
+        //do not render crosshair
+        PlayerData currentData = PlayerDataManager.localPlayer;
+        if (currentData != null && currentData.script != null && !currentData.script.crossHairEnabled)
+            ci.cancel();
     }
 
-    @Redirect(
+    @ModifyArgs(
             method = "renderCrosshair",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V"
-            ),
-            slice = @Slice(
-                    from = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;blendFuncSeparate(Lcom/mojang/blaze3d/platform/GlStateManager$SrcFactor;Lcom/mojang/blaze3d/platform/GlStateManager$DstFactor;Lcom/mojang/blaze3d/platform/GlStateManager$SrcFactor;Lcom/mojang/blaze3d/platform/GlStateManager$DstFactor;)V"),
-                    to = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;getAttackCooldownProgress(F)F")
+                    target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V",
+                    ordinal = 0
             )
     )
-    private void renderCrosshair(InGameHud inGameHud, MatrixStack matrices, int x, int y, int u, int v, int width, int height) {
+    private void renderCrosshairDrawTexture(Args args) {
+        //set crosshair offset
         PlayerData currentData = PlayerDataManager.localPlayer;
-        if (currentData != null && currentData.script != null) {
-
-            //doesnt render crosshair
-            if (!currentData.script.crossHairEnabled) return;
-
-            //crosshair offset
-            if (currentData.script.crossHairPos != null) {
-                x += currentData.script.crossHairPos.x;
-                y += currentData.script.crossHairPos.y;
-            }
+        if (currentData != null && currentData.script != null && currentData.script.crossHairPos != null) {
+            args.set(1, (int) ((int) args.get(1) + currentData.script.crossHairPos.x));
+            args.set(2, (int) ((int) args.get(2) + currentData.script.crossHairPos.y));
         }
-
-        //render crosshair
-        inGameHud.drawTexture(matrices, x, y, u, v, width, height);
     }
 }
