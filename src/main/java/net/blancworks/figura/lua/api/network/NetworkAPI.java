46f52f04525cc14fa04701596a7173792c205951
Fran <40085587+Francy-chan@users.noreply.github.com>
2021-11-09 06:39:09 -0300

    Update NetworkAPI.java

--- src/main/java/net/blancworks/figura/lua/api/network/NetworkAPI.java
+++ src/main/java/net/blancworks/figura/lua/api/network/NetworkAPI.java
@@ -4,6 +4,7 @@ import net.blancworks.figura.PlayerDataManager;
 import net.blancworks.figura.lua.CustomScript;
 import net.blancworks.figura.lua.api.ReadOnlyLuaTable;
 import net.minecraft.util.Identifier;
+import org.luaj.vm2.LuaError;
 import org.luaj.vm2.LuaTable;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.OneArgFunction;
@@ -40,26 +41,29 @@ public class NetworkAPI {
             table.set("ping", new TwoArgFunction() {
                 @Override
                 public LuaValue call(LuaValue arg1, LuaValue arg2) {
-                    
                     //Only allow sending pings on local player avatar.
                     if(targetScript.playerData != PlayerDataManager.localPlayer)
                         return NIL;
-                    
-                    String s = arg1.checkjstring();
-                    
-                    //Get ID from string.
-                    short id = targetScript.functionIDMap.inverse().get(s);
-                    
-                    //Script handles local ping immediately.
-                    targetScript.handlePing(id, arg2);
 
-                    CustomScript.LuaPing lp = new CustomScript.LuaPing();
-                    lp.args = arg2;
-                    lp.functionID = id;
-                    
-                    if(!targetScript.playerData.isLocalAvatar)
-                        targetScript.outgoingPingQueue.add(lp);
-                    
+                    try {
+                        String s = arg1.checkjstring();
+
+                        //Get ID from string.
+                        short id = targetScript.functionIDMap.inverse().get(s);
+
+                        //Script handles local ping immediately.
+                        targetScript.handlePing(id, arg2);
+
+                        CustomScript.LuaPing lp = new CustomScript.LuaPing();
+                        lp.args = arg2;
+                        lp.functionID = id;
+
+                        if (!targetScript.playerData.isLocalAvatar)
+                            targetScript.outgoingPingQueue.add(lp);
+                    } catch (Exception ignored) {
+                        throw new LuaError("Failed to send ping! Make sure the ping is registered before sending it!");
+                    }
+
                     return NIL;
                 }
             });
