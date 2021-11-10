1efbf4226e2694d5867f10dfa16e0051f4efadda
Fran <40085587+Francy-chan@users.noreply.github.com>
2021-11-10 04:15:35 -0300

    fixed stuff

--- src/main/java/net/blancworks/figura/mixin/InGameHudMixin.java
+++ src/main/java/net/blancworks/figura/mixin/InGameHudMixin.java
@@ -6,11 +6,13 @@ import net.blancworks.figura.PlayerDataManager;
 import net.blancworks.figura.gui.ActionWheel;
 import net.blancworks.figura.gui.PlayerPopup;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
@@ -27,6 +29,11 @@ public abstract class InGameHudMixin {
     public void postRender(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
         if (FiguraMod.actionWheel.isPressed())
             ActionWheel.render(matrices);
+    }
+
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/KeyBinding;isPressed()Z"))
+    private boolean onPlayerListRender(KeyBinding keyPlayerList) {
+        return keyPlayerList.isPressed() || PlayerPopup.miniEnabled;
     }
 
     @Inject(at = @At ("HEAD"), method = "renderCrosshair", cancellable = true)
--- src/main/java/net/blancworks/figura/mixin/PlayerListHudMixin.java
+++ src/main/java/net/blancworks/figura/mixin/PlayerListHudMixin.java
@@ -82,19 +82,17 @@ public class PlayerListHudMixin {
     private void render(MatrixStack matrices, int scaledWindowWidth, Scoreboard scoreboard, ScoreboardObjective objective, CallbackInfo ci, ClientPlayNetworkHandler clientPlayNetworkHandler, List<?> list, int i, int j, int l, int m, int n, boolean bl, int q, int r, int s, int t, int u, List<?> list2, List<?> list3, int w, int x, int y, int z, int aa, int ab, PlayerListEntry playerListEntry2, GameProfile gameProfile, PlayerEntity playerEntity, boolean bl2, int ae, int af) {
         this.playerEntity = playerEntity;
 
-        if (FiguraMod.playerPopup.isPressed()) {
-            if (x == PlayerPopup.miniSelected && PlayerPopup.miniExchange) {
-                PlayerPopup.entity = MinecraftClient.getInstance().world.getPlayerByUuid(playerListEntry2.getProfile().getId());
+        if (PlayerPopup.miniEnabled && x == PlayerPopup.miniSelected) {
+            PlayerPopup.entity = MinecraftClient.getInstance().world.getPlayerByUuid(playerListEntry2.getProfile().getId());
 
-                PlayerPopup.miniSize = list.size();
+            PlayerPopup.miniSize = list.size();
 
-                matrices.push();
-                matrices.translate(aa, ab, 0f);
-                PlayerPopup.renderMini(matrices);
-                matrices.pop();
+            matrices.push();
+            matrices.translate(aa, ab, 0f);
+            PlayerPopup.renderMini(matrices);
+            matrices.pop();
 
-                RenderSystem.setShaderTexture(0, playerListEntry2.getSkinTexture());
-            }
+            RenderSystem.setShaderTexture(0, playerListEntry2.getSkinTexture());
         }
     }
 
