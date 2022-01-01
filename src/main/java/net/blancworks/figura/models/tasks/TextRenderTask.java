b56e2fe41f3b63a53fce57e29230decbf9174ec6
omoflop <29988852+omoflop@users.noreply.github.com>
2022-01-01 14:31:08 -0800

    Added newline support to text render tasks

--- src/main/java/net/blancworks/figura/models/tasks/TextRenderTask.java
+++ src/main/java/net/blancworks/figura/models/tasks/TextRenderTask.java
@@ -1,5 +1,6 @@
 package net.blancworks.figura.models.tasks;
 
+import net.blancworks.figura.utils.TextUtils;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -7,13 +8,16 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.Vec3f;
 
+import java.util.List;
+
 public class TextRenderTask extends RenderTask {
     private final TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
-    public Text text;
+    public List<Text> text;
+    public int lineSpacing = 9;
 
     public TextRenderTask(Text text, boolean emissive, Vec3f pos, Vec3f rot, Vec3f scale) {
         super(emissive, pos, rot, scale);
-        this.text = text;
+        this.text = TextUtils.splitText(text, "\n");
     }
 
     @Override
@@ -23,7 +27,11 @@ public class TextRenderTask extends RenderTask {
         this.transform(matrices);
         matrices.scale(0.025f, 0.025f, 0.025f);
 
-        int instructions = textRenderer.draw(text, 0, 0, 0xFFFFFF, false, matrices.peek().getModel(), vcp, false, 0, emissive ? 0xF000F0 : light);
+        int instructions = 0;
+
+        for(int i = 0; i < text.size(); i++) {
+            textRenderer.draw(text.get(i), 0, i * lineSpacing, 0xFFFFFF, false, matrices.peek().getModel(), vcp, false, 0, emissive ? 0xF000F0 : light);
+        }
 
         matrices.pop();
         return instructions;
