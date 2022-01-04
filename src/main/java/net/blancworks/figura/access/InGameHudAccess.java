2f8a6ce27a695e45ccb76dbf675090e8861df0f2
omoflop <29988852+omoflop@users.noreply.github.com>
2022-01-03 19:09:33 -0800

    Added client.setTitle, and other related functions
    ### Client API ###
    + setTitle(text)
    + getTitle()
    + setSubtitle(text)
    + getSubtitle()
    + setActionbar(text)
    + getActionbar()
    + setTitleTimes(fadeIn,stay,fadeOut)
    + clearTitle()

---
+++ src/main/java/net/blancworks/figura/access/InGameHudAccess.java
@@ -0,0 +1,9 @@
+package net.blancworks.figura.access;
+
+import net.minecraft.text.Text;
+
+public interface InGameHudAccess {
+    Text getTitle();
+    Text getSubtitle();
+    Text getOverlayMessage();
+}
