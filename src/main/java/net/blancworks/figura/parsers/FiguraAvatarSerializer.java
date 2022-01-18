5dbe8503e0759dc9a82bc9ac683166436850baba
Fran <franngoesa@gmail.com>
2022-01-18 19:49:02 -0300

    bump ver and save/cache buttons

--- src/main/java/net/blancworks/figura/parsers/FiguraAvatarSerializer.java
+++ src/main/java/net/blancworks/figura/parsers/FiguraAvatarSerializer.java
@@ -18,203 +18,163 @@ import java.util.*;
 
 public class FiguraAvatarSerializer {
 
-    public static void serialize(NbtCompound nbt) {
-        FiguraMod.doTask(() -> {
+    public static String serialize(NbtCompound nbt) {
+        try {
+            String fileName = "avatar-" + new SimpleDateFormat("yyyy_MM_dd-HH_mm_ss").format(new Date());
+            Path rootFolder = FiguraMod.getModContentDirectory().resolve("model_files/[§9Figura§r] Saved Avatars");
+            Path dest = rootFolder.resolve(Path.of(fileName));
+            if (!Files.exists(dest))
+                Files.createDirectories(dest);
+            boolean hasTexture = false;
+            int stuff = 0;
+            int success = 0;
+            //script
             try {
-                String fileName = "avatar-" + new SimpleDateFormat("yyyy_MM_dd-HH_mm_ss").format(new Date());
-                Path rootFolder = FiguraMod.getModContentDirectory().resolve("model_files/[§9Figura§r] Saved Avatars");
-                Path dest = rootFolder.resolve(Path.of(fileName));
-
-                if (!Files.exists(dest))
-                    Files.createDirectories(dest);
-
-                boolean hasTexture = false;
-                int stuff = 0;
-                int success = 0;
-
-                //script
-                try {
-                    if (nbt.contains("script")) {
-                        stuff++;
-
-                        Path script = dest.resolve("script.lua");
-                        NbtCompound tag = nbt.getCompound("script");
-
-                        String source;
-
-                        Set<String> keys = tag.getKeys();
-                        if (keys.size() <= 1) {
-                            source = tag.getString("src");
-                        } else {
-                            StringBuilder src = new StringBuilder();
-                            for (int i = 0; i < keys.size(); i++) {
-                                src.append(tag.getString("src_" + i));
-                            }
-                            source = src.toString();
+                if (nbt.contains("script")) {
+                    stuff++;
+                    Path script = dest.resolve("script.lua");
+                    NbtCompound tag = nbt.getCompound("script");
+                    String source;
+                    Set<String> keys = tag.getKeys();
+                    if (keys.size() <= 1) {
+                        source = tag.getString("src");
+                    } else {
+                        StringBuilder src = new StringBuilder();
+                        for (int i = 0; i < keys.size(); i++) {
+                            src.append(tag.getString("src_" + i));
                         }
-
-                        Files.writeString(script, source);
-                        success++;
+                        source = src.toString();
                     }
-                } catch (Exception e) {
-                    FiguraMod.LOGGER.error(e);
+                    Files.writeString(script, source);
+                    success++;
                 }
-
-                //sounds
-                try {
-                    if (nbt.contains("sounds")) {
-                        stuff++;
-
-                        Path sounds = dest.resolve("sounds.json");
-                        Path soundFolder = dest.resolve("sounds");
-
-                        NbtCompound tag = nbt.getCompound("sounds");
-                        JsonArray soundsList = new JsonArray();
-
-                        if (!Files.exists(soundFolder))
-                            Files.createDirectories(soundFolder);
-
-                        Set<String> keys = tag.getKeys();
-                        for (String s : keys) {
-                            soundsList.add(s);
-
-                            Path sound = soundFolder.resolve(s + ".ogg");
-                            Files.write(sound, tag.getByteArray(s));
-                        }
-
-                        //save sounds json
-                        String jsonString = new GsonBuilder().create().toJson(soundsList);
-                        Files.writeString(sounds, jsonString);
-
-                        success++;
-                    }
-                } catch (Exception e) {
-                    FiguraMod.LOGGER.error(e);
-                }
-
-                //texture
-                try {
-                    if (nbt.contains("texture")) {
-                        stuff++;
-
-                        NbtCompound tag = nbt.getCompound("texture");
-
-                        String type = tag.getString("type");
-                        if (type.equals("color")) type = "";
-
-                        Path texture = dest.resolve("texture" + type + ".png");
-                        Files.write(texture, tag.getByteArray("img2"));
-
-                        hasTexture = true;
-                        success++;
-                    }
-                } catch (Exception e) {
-                    FiguraMod.LOGGER.error(e);
-                }
-
-                //extra textures
-                try {
-                    if (nbt.contains("exTexs")) {
-                        stuff++;
-
-                        NbtList list = nbt.getList("exTexs", NbtElement.COMPOUND_TYPE);
-
-                        for (NbtElement tag : list) {
-                            String type = ((NbtCompound) tag).getString("type");
-                            if (type.equals("color")) type = "";
-
-                            Path texture = dest.resolve("texture" + type + ".png");
-                            Files.write(texture, ((NbtCompound) tag).getByteArray("img2"));
-                        }
-
-                        success++;
-                    }
-                } catch (Exception e) {
-                    FiguraMod.LOGGER.error(e);
-                }
-
-                //model
-                try {
-                    if (nbt.contains("model")) {
-                        stuff++;
-
-                        JsonObject model = new JsonObject();
-                        NbtList list = nbt.getCompound("model").getList("parts", NbtElement.COMPOUND_TYPE);
-
-                        //header
-                        JsonObject meta = new JsonObject();
-                        meta.addProperty("format_version", "4.0");
-                        meta.addProperty("creation_time", System.currentTimeMillis());
-                        meta.addProperty("model_format", "free");
-                        meta.addProperty("box_uv", false);
-
-                        model.add("meta", meta);
-                        model.addProperty("name", "");
-                        model.addProperty("geometry_name", "");
-
-                        JsonArray box = new JsonArray();
-                        box.add(1); box.add(1); box.add(0);
-                        model.add("visible_box", box);
-
-                        //texture
-                        if (hasTexture) {
-                            JsonArray textures = new JsonArray();
-                            JsonObject texture = new JsonObject();
-                            byte[] data = nbt.getCompound("texture").getByteArray("img2");
-
-                            texture.addProperty("path", dest.resolve("texture.png").toString());
-                            texture.addProperty("name", "texture.png");
-                            texture.addProperty("folder", fileName);
-                            texture.addProperty("namespace", "");
-                            texture.addProperty("id", "0");
-                            texture.addProperty("particle", false);
-                            texture.addProperty("render_mode", "normal");
-                            texture.addProperty("visible", true);
-                            texture.addProperty("mode", "bitmap");
-                            texture.addProperty("saved", true);
-                            texture.addProperty("uuid", UUID.randomUUID().toString());
-                            texture.addProperty("relative_path", "../texture.png");
-                            texture.addProperty("source", "data:image/png;base64," + Base64.getEncoder().encodeToString(data));
-                            textures.add(texture);
-
-                            model.add("textures", textures);
-                        }
-
-                        //texture size
-                        JsonObject resolution = new JsonObject();
-                        NbtList uv = nbt.getCompound("model").getList("uv", NbtElement.FLOAT_TYPE);
-                        resolution.addProperty("width", uv.getFloat(0));
-                        resolution.addProperty("height", uv.getFloat(1));
-
-                        model.add("resolution", resolution);
-
-                        //the model
-                        boolean isPlayer = false;
-                        JsonArray outliner = new JsonArray();
-                        JsonArray elements = new JsonArray();
-                        for (NbtElement tagElement : list) {
-                            isPlayer = parseModelPart((NbtCompound) tagElement, elements, outliner, Vec3f.ZERO.copy()) || isPlayer;
-                        }
-
-                        model.add("elements", elements);
-                        model.add("outliner", outliner);
-
-                        //write model
-                        String jsonString = new GsonBuilder().serializeNulls().create().toJson(model);
-                        Files.writeString(dest.resolve((isPlayer ? "player_model" : "model") + ".bbmodel"), jsonString);
-
-                        success++;
-                    }
-                } catch (Exception e) {
-                    FiguraMod.LOGGER.error(e);
-                }
-
-                FiguraMod.sendToast("done", success + " / " + stuff);
             } catch (Exception e) {
                 FiguraMod.LOGGER.error(e);
-                FiguraMod.sendToast("oopsie", "Something went really wrong");
             }
-        });
+            //sounds
+            try {
+                if (nbt.contains("sounds")) {
+                    stuff++;
+                    Path sounds = dest.resolve("sounds.json");
+                    Path soundFolder = dest.resolve("sounds");
+                    NbtCompound tag = nbt.getCompound("sounds");
+                    JsonArray soundsList = new JsonArray();
+                    if (!Files.exists(soundFolder))
+                        Files.createDirectories(soundFolder);
+                    Set<String> keys = tag.getKeys();
+                    for (String s : keys) {
+                        soundsList.add(s);
+                        Path sound = soundFolder.resolve(s + ".ogg");
+                        Files.write(sound, tag.getByteArray(s));
+                    }
+                    //save sounds json
+                    String jsonString = new GsonBuilder().create().toJson(soundsList);
+                    Files.writeString(sounds, jsonString);
+                    success++;
+                }
+            } catch (Exception e) {
+                FiguraMod.LOGGER.error(e);
+            }
+            //texture
+            try {
+                if (nbt.contains("texture")) {
+                    stuff++;
+                    NbtCompound tag = nbt.getCompound("texture");
+                    String type = tag.getString("type");
+                    if (type.equals("color")) type = "";
+                    Path texture = dest.resolve("texture" + type + ".png");
+                    Files.write(texture, tag.getByteArray("img2"));
+                    hasTexture = true;
+                    success++;
+                }
+            } catch (Exception e) {
+                FiguraMod.LOGGER.error(e);
+            }
+            //extra textures
+            try {
+                if (nbt.contains("exTexs")) {
+                    stuff++;
+                    NbtList list = nbt.getList("exTexs", NbtElement.COMPOUND_TYPE);
+                    for (NbtElement tag : list) {
+                        String type = ((NbtCompound) tag).getString("type");
+                        if (type.equals("color")) type = "";
+                        Path texture = dest.resolve("texture" + type + ".png");
+                        Files.write(texture, ((NbtCompound) tag).getByteArray("img2"));
+                    }
+                    success++;
+                }
+            } catch (Exception e) {
+                FiguraMod.LOGGER.error(e);
+            }
+            //model
+            try {
+                if (nbt.contains("model")) {
+                    stuff++;
+                    JsonObject model = new JsonObject();
+                    NbtList list = nbt.getCompound("model").getList("parts", NbtElement.COMPOUND_TYPE);
+                    //header
+                    JsonObject meta = new JsonObject();
+                    meta.addProperty("format_version", "4.0");
+                    meta.addProperty("creation_time", System.currentTimeMillis());
+                    meta.addProperty("model_format", "free");
+                    meta.addProperty("box_uv", false);
+                    model.add("meta", meta);
+                    model.addProperty("name", "");
+                    model.addProperty("geometry_name", "");
+                    JsonArray box = new JsonArray();
+                    box.add(1); box.add(1); box.add(0);
+                    model.add("visible_box", box);
+                    //texture
+                    if (hasTexture) {
+                        JsonArray textures = new JsonArray();
+                        JsonObject texture = new JsonObject();
+                        byte[] data = nbt.getCompound("texture").getByteArray("img2");
+                        texture.addProperty("path", dest.resolve("texture.png").toString());
+                        texture.addProperty("name", "texture.png");
+                        texture.addProperty("folder", fileName);
+                        texture.addProperty("namespace", "");
+                        texture.addProperty("id", "0");
+                        texture.addProperty("particle", false);
+                        texture.addProperty("render_mode", "normal");
+                        texture.addProperty("visible", true);
+                        texture.addProperty("mode", "bitmap");
+                        texture.addProperty("saved", true);
+                        texture.addProperty("uuid", UUID.randomUUID().toString());
+                        texture.addProperty("relative_path", "../texture.png");
+                        texture.addProperty("source", "data:image/png;base64," + Base64.getEncoder().encodeToString(data));
+                        textures.add(texture);
+                        model.add("textures", textures);
+                    }
+                    //texture size
+                    JsonObject resolution = new JsonObject();
+                    NbtList uv = nbt.getCompound("model").getList("uv", NbtElement.FLOAT_TYPE);
+                    resolution.addProperty("width", uv.getFloat(0));
+                    resolution.addProperty("height", uv.getFloat(1));
+                    model.add("resolution", resolution);
+                    //the model
+                    boolean isPlayer = false;
+                    JsonArray outliner = new JsonArray();
+                    JsonArray elements = new JsonArray();
+                    for (NbtElement tagElement : list) {
+                        isPlayer = parseModelPart((NbtCompound) tagElement, elements, outliner, Vec3f.ZERO.copy()) || isPlayer;
+                    }
+                    model.add("elements", elements);
+                    model.add("outliner", outliner);
+                    //write model
+                    String jsonString = new GsonBuilder().serializeNulls().create().toJson(model);
+                    Files.writeString(dest.resolve((isPlayer ? "player_model" : "model") + ".bbmodel"), jsonString);
+                    success++;
+                }
+            } catch (Exception e) {
+                FiguraMod.LOGGER.error(e);
+            }
+
+            return success + " / " + stuff;
+        } catch (Exception e) {
+            FiguraMod.LOGGER.error(e);
+        }
+
+        return null;
     }
 
     public static final HashMap<String, Vec3f> PLAYER_MODEL_OFFSETS = new HashMap<>() {{
