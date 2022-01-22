15351d3d95489c22f46a43d4d2b3e9eb46a12995
Fran <franngoesa@gmail.com>
2022-01-22 00:48:59 -0300

    fixed #195

--- src/main/java/net/blancworks/figura/avatar/LocalAvatarData.java
+++ src/main/java/net/blancworks/figura/avatar/LocalAvatarData.java
@@ -382,7 +382,7 @@ public class LocalAvatarData extends AvatarData {
                     }
                 });
             });
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
